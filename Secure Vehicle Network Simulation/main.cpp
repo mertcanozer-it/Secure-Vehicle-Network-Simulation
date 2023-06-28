35b646543677f53f5273740e603d67ba69a3a8b2
mertcanozer-it <mertcanmakiozer@outlook.de>
2023-06-29 00:50:28 +0900

    Starting of simple simulation of a network of vehicles with basic security checks. In this code, Vehicle 1 trusts Vehicle 2. When they send messages to each other, the messages are accepted and printed.

--- Secure Vehicle Network Simulation/main.cpp
+++ Secure Vehicle Network Simulation/main.cpp
@@ -1,14 +1,37 @@
-//
-//  main.cpp
-//  Secure Vehicle Network Simulation
-//
-//  Created by Mertcan Ozer on 2023/06/29.
-//
-
 #include <iostream>
+#include <string>
+#include <unordered_set>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class Vehicle {
+public:
+    Vehicle(int id, const std::unordered_set<int>& trusted_ids)
+        : id(id), trusted_ids(trusted_ids) {}
+
+    void send(Vehicle& other, const std::string& message) {
+        other.receive(id, message);
+    }
+
+    void receive(int sender_id, const std::string& message) {
+        if (trusted_ids.find(sender_id) != trusted_ids.end()) {
+            std::cout << "Vehicle " << id << " received: " << message << std::endl;
+        } else {
+            std::cout << "Vehicle " << id << " received unauthorized message from " << sender_id << std::endl;
+        }
+    }
+
+private:
+    int id;
+    std::unordered_set<int> trusted_ids;
+};
+
+int main() {
+    std::unordered_set<int> trusted_ids_1 = {2};
+    std::unordered_set<int> trusted_ids_2 = {1};
+    Vehicle vehicle1(1, trusted_ids_1);
+    Vehicle vehicle2(2, trusted_ids_2);
+
+    vehicle1.send(vehicle2, "Hello from vehicle 1!");
+    vehicle2.send(vehicle1, "Hello from vehicle 2!");
+
     return 0;
 }
