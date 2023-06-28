7fae23feb1b0d345e2fa28fb8aae063a06220510
mertcanozer-it <mertcanmakiozer@outlook.de>
2023-06-29 02:32:30 +0900

    For the possibility of replay attacks, incrementing sequence number with each message has been included. Before it verifies a message's signature, it checks the sequence number to make sure the message is not a replay.

--- Secure Vehicle Network Simulation/main.cpp
+++ Secure Vehicle Network Simulation/main.cpp
@@ -5,17 +5,23 @@
 class Vehicle {
 public:
     Vehicle(int id, int key, const std::unordered_map<int, int>& trusted_keys)
-        : id(id), key(key), trusted_keys(trusted_keys) {}
+        : id(id), key(key), trusted_keys(trusted_keys), sequence_number(0) {}
 
     void send(Vehicle& other, const std::string& message) {
-        int signature = sign(message);
-        other.receive(id, message, signature);
+        int signature = sign(message, sequence_number);
+        other.receive(id, message, signature, sequence_number);
+        sequence_number++;
     }
 
-    void receive(int sender_id, const std::string& message, int signature) {
+    void receive(int sender_id, const std::string& message, int signature, int sequence_number) {
         auto it = trusted_keys.find(sender_id);
         if (it != trusted_keys.end()) {
-            if (verify(message, signature, it->second)) {
+            if (sequence_number <= last_sequence_numbers[sender_id]) {
+                std::cout << "Vehicle " << id << " disregarded replayed message from " << sender_id << std::endl;
+                return;
+            }
+            last_sequence_numbers[sender_id] = sequence_number;
+            if (verify(message, signature, sequence_number, it->second)) {
                 std::cout << "Vehicle " << id << " received: " << message << std::endl;
             } else {
                 std::cout << "Vehicle " << id << " received forged message from " << sender_id << std::endl;
@@ -29,21 +35,23 @@ private:
     int id;
     int key;
     std::unordered_map<int, int> trusted_keys;
+    std::unordered_map<int, int> last_sequence_numbers;
+    int sequence_number;
 
-    int sign(const std::string& message) {
+    int sign(const std::string& message, int sequence_number) {
         int sum = 0;
         for (char c : message) {
             sum += c;
         }
-        return sum * key;
+        return (sum + sequence_number) * key;
     }
 
-    bool verify(const std::string& message, int signature, int sender_key) {
+    bool verify(const std::string& message, int signature, int sequence_number, int sender_key) {
         int sum = 0;
         for (char c : message) {
             sum += c;
         }
-        return signature == sum * sender_key;
+        return signature == (sum + sequence_number) * sender_key;
     }
 };
 
@@ -56,11 +64,9 @@ int main() {
     vehicle1.send(vehicle2, "Hello from vehicle 1!");
     vehicle2.send(vehicle1, "Hello from vehicle 2!");
 
-    // Creating a third vehicle that is not trusted by either vehicle 1 or vehicle 2
     std::unordered_map<int, int> trusted_keys_3 = {};
     Vehicle vehicle3(3, 5, trusted_keys_3);
 
-    // Vehicle 3 attempts to send messages to vehicle 1 and vehicle 2
     vehicle3.send(vehicle1, "Hello from vehicle 3!");
     vehicle3.send(vehicle2, "Hello from vehicle 3!");
 
