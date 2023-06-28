7fe8469523469783f47a3cdf0e5e53842c3f2efb
mertcanozer-it <mertcanmakiozer@outlook.de>
2023-06-29 01:33:24 +0900

    vehicle3 is not trusted by either vehicle1 or vehicle2, and its attempts to send messages to them are labeled as unauthorized. However, even if vehicle3 were to somehow learn the keys of vehicle1 and vehicle2 and try to send a message pretending to be a trusted vehicle, the signature verification would fail, and the message would be labeled as forged. because each vehicle now has a cryptographic key and a list of trusted keys. When a vehicle sends a message, it includes a signature, which is the "encrypted" version of the message. When a vehicle receives a message, it first checks if the sender is in its list of trusted keys. If it is, it then verifies the signature by decrypting it with the sender's public key and checking ifit matches the message. If the signature doesn't match, it knows the message is forged.

--- Secure Vehicle Network Simulation/main.cpp
+++ Secure Vehicle Network Simulation/main.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <string>
-#include <unordered_set>
+#include <unordered_map>
 
 class Vehicle {
 public:
-    Vehicle(int id, const std::unordered_set<int>& trusted_ids)
-        : id(id), trusted_ids(trusted_ids) {}
+    Vehicle(int id, int key, const std::unordered_map<int, int>& trusted_keys)
+        : id(id), key(key), trusted_keys(trusted_keys) {}
 
     void send(Vehicle& other, const std::string& message) {
-        other.receive(id, message);
+        int signature = sign(message);
+        other.receive(id, message, signature);
     }
 
-    void receive(int sender_id, const std::string& message) {
-        if (trusted_ids.find(sender_id) != trusted_ids.end()) {
-            std::cout << "Vehicle " << id << " received: " << message << std::endl;
+    void receive(int sender_id, const std::string& message, int signature) {
+        auto it = trusted_keys.find(sender_id);
+        if (it != trusted_keys.end()) {
+            if (verify(message, signature, it->second)) {
+                std::cout << "Vehicle " << id << " received: " << message << std::endl;
+            } else {
+                std::cout << "Vehicle " << id << " received forged message from " << sender_id << std::endl;
+            }
         } else {
             std::cout << "Vehicle " << id << " received unauthorized message from " << sender_id << std::endl;
         }
@@ -21,21 +27,38 @@ public:
 
 private:
     int id;
-    std::unordered_set<int> trusted_ids;
+    int key;
+    std::unordered_map<int, int> trusted_keys;
+
+    int sign(const std::string& message) {
+        int sum = 0;
+        for (char c : message) {
+            sum += c;
+        }
+        return sum * key;
+    }
+
+    bool verify(const std::string& message, int signature, int sender_key) {
+        int sum = 0;
+        for (char c : message) {
+            sum += c;
+        }
+        return signature == sum * sender_key;
+    }
 };
 
 int main() {
-    std::unordered_set<int> trusted_ids_1 = {2};
-    std::unordered_set<int> trusted_ids_2 = {1};
-    Vehicle vehicle1(1, trusted_ids_1);
-    Vehicle vehicle2(2, trusted_ids_2);
-
-    // Creating a third vehicle that is not trusted by either vehicle 1 or vehicle 2
-    std::unordered_set<int> trusted_ids_3 = {};
-    Vehicle vehicle3(3, trusted_ids_3);
+    std::unordered_map<int, int> trusted_keys_1 = {{2, 3}};
+    std::unordered_map<int, int> trusted_keys_2 = {{1, 2}};
+    Vehicle vehicle1(1, 2, trusted_keys_1);
+    Vehicle vehicle2(2, 3, trusted_keys_2);
 
     vehicle1.send(vehicle2, "Hello from vehicle 1!");
     vehicle2.send(vehicle1, "Hello from vehicle 2!");
+
+    // Creating a third vehicle that is not trusted by either vehicle 1 or vehicle 2
+    std::unordered_map<int, int> trusted_keys_3 = {};
+    Vehicle vehicle3(3, 5, trusted_keys_3);
 
     // Vehicle 3 attempts to send messages to vehicle 1 and vehicle 2
     vehicle3.send(vehicle1, "Hello from vehicle 3!");
