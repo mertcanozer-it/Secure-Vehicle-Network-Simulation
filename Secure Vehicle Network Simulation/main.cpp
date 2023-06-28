0d3e24837c0e6e7d8dc9f28932f8afef04053018
mertcanozer-it <mertcanmakiozer@outlook.de>
2023-06-29 00:54:21 +0900

    Created a third vehicle, vehicle3, which is not trusted by vehicle1 or vehicle2. When vehicle3 attempts to send messages to vehicle1 and vehicle2, receivers recognizes that vehicle3 is not in their list of trusted IDs, and thus the messages are labeled as unauthorized.

--- Secure Vehicle Network Simulation/main.cpp
+++ Secure Vehicle Network Simulation/main.cpp
@@ -30,8 +30,16 @@ int main() {
     Vehicle vehicle1(1, trusted_ids_1);
     Vehicle vehicle2(2, trusted_ids_2);
 
+    // Creating a third vehicle that is not trusted by either vehicle 1 or vehicle 2
+    std::unordered_set<int> trusted_ids_3 = {};
+    Vehicle vehicle3(3, trusted_ids_3);
+
     vehicle1.send(vehicle2, "Hello from vehicle 1!");
     vehicle2.send(vehicle1, "Hello from vehicle 2!");
+
+    // Vehicle 3 attempts to send messages to vehicle 1 and vehicle 2
+    vehicle3.send(vehicle1, "Hello from vehicle 3!");
+    vehicle3.send(vehicle2, "Hello from vehicle 3!");
 
     return 0;
 }
